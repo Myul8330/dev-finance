28b1180beb51aee2410a8f938e4b018ccf9b815f
Thiago Da Silva <83181237+Myul8330@users.noreply.github.com>
2021-05-12 21:01:57 -0300

    add Scripts.js

---
+++ Scripts.js
@@ -0,0 +1,24 @@
+/*const Modal = {
+    open(){
+        //Abrir Modal
+        //Adicionar a class active ao 
+        document
+                .querySelector('.modal-overlay')
+                .classList
+                .add('active');
+    },
+    close(){
+            //fecha o modal
+            //remove a class active do modal
+            document
+                .querySelector('modal-overlay')
+                .classList
+                .remove('active');
+    }
+}*/
+function open()  {
+    document.querySelector('.modal-overlay');
+
+function open(x) {
+    x.classList.toggle("change");
+  }
